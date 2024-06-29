16685102066e0347e81426b299aa60fdfbc68d7b
Frithjof <fmpetrick@gmail.com>
2024-06-29 14:02:05 +0200

    load exercise list from separate file

---
+++ content.js
@@ -0,0 +1,119 @@
+const exerciseList = [
+  {
+    "question_type": "reading",
+    "question": "爸爸为什么来日本？",
+    "answer_options": [
+      {
+        "chinese": "a. 过生日"
+      },
+      {
+        "chinese": "b. 看瑞典的朋友"
+      },
+      {
+        "chinese": "c. 过妈妈的生日",
+        "correct": true
+      },
+      {
+        "chinese": "d. 买东西"
+      }
+    ]
+  },
+  {
+    "question_type": "reading",
+    "question": "谁住在瑞典？",
+    "answer_options": [
+      {
+        "chinese": "a. 西西"
+      },
+      {
+        "chinese": "b. 高小月"
+      },
+      {
+        "chinese": "c. 大北",
+        "correct": true
+      },
+      {
+        "chinese": "d. 妈妈"
+      }
+    ]
+  },
+  {
+    "question_type": "reading",
+    "question": "为什么西西的妈妈很高兴？",
+    "answer_options": [
+      {
+        "chinese": "a. 因为她有很多猫"
+      },
+      {
+        "chinese": "b. 因为她的生日是明天"
+      },
+      {
+        "chinese": "c. 因为她的男朋友来了",
+        "correct": true
+      },
+      {
+        "chinese": "d. 因为她在瑞典住"
+      }
+    ]
+  },
+  {
+    "question_type": "grammar",
+    "english": "Mom is there smiling.",
+    "grammar_point": {
+      "identifier": "zhe",
+      "name": "Continuous action or state with 着"
+    },
+    "word_bank": [
+      "妈妈",
+      "在",
+      "那儿",
+      "笑",
+      "着",
+      "。"
+    ],
+    "solutions": [
+      "妈妈在那儿笑着。"
+    ]
+  },
+  {
+    "question_type": "grammar",
+    "english": "My name is Xi Xi, and I am also a cat.",
+    "grammar_point": {
+      "identifier": "ye",
+      "name": "也"
+    },
+    "word_bank": [
+      "我",
+      "叫",
+      "西西",
+      "，",
+      "我",
+      "也",
+      "是",
+      "猫",
+      "。"
+    ],
+    "solutions": [
+      "我叫西西，我也是猫。"
+    ]
+  },
+  {
+    "question_type": "grammar",
+    "english": "I am also smiling.",
+    "grammar_point": {
+      "identifier": "zhe",
+      "name": "Continuous action or state with 着"
+    },
+    "word_bank": [
+      "我",
+      "也",
+      "在",
+      "笑",
+      "着",
+      "。"
+    ],
+    "solutions": [
+      "我也在笑着。"
+    ]
+  }
+]
