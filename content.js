f81070371173a17e2f7a9a6b6c6d7c313d1fcc47
Frithjof <fmpetrick@gmail.com>
2024-07-08 10:09:38 +0200

    content.js, update

--- content.js
+++ content.js
@@ -1,136 +1,757 @@
-const exerciseList = [
-  {
-    "question_type": "reading",
-    "question": "爸爸为什么来日本？",
-    "answer_options": [
-      "a. 过生日",
-      "b. 看瑞典的朋友",
-      "c. 过妈妈的生日",
-      "d. 买东西"
-    ],
-    "solution": 2
-  },
-  {
-    "question_type": "reading",
-    "question": "谁住在瑞典？",
-    "answer_options": [
-      "a. 西西",
-      "b. 高小月",
-      "c. 大北",
-      "d. 妈妈"
-    ],
-    "solution": 2
-  },
-  {
-    "question_type": "reading",
-    "question": "为什么西西的妈妈很高兴？",
-    "answer_options": [
-      "a. 因为她有很多猫",
-      "b. 因为她的生日是明天",
-      "c. 因为她的男朋友来了",
-      "d. 因为她在瑞典住"
-    ],
-    "solution": 2
-  },
-  {
-    "question_type": "grammar",
-    "english": "Mom is there smiling.",
-    "grammar_point": {
-      "slug": "zhe",
-      "name": "Continuous action or state with 着"
+const exercisesByLessonId = {
+  "1580": [
+    {
+      "question_type": "reading",
+      "question": "有些奶茶店为什么赚不到钱?",
+      "answer_options": [
+        "a. 因为奶茶太贵",
+        "b. 因为奶茶里没有咖啡因",
+        "c. 因为奶茶店选择的地点不好",
+        "d. 因为没有人喜欢奶茶"
+      ],
+      "solution": 2
     },
-    "word_bank": [
-      "笑",
-      "在",
-      "妈妈",
-      "着",
-      "那儿"
-    ],
-    "prefilled_postfix": ["。"],
-    "solutions": [
-      ["妈妈", "在", "那儿", "笑", "着", "。"]
-    ]
-  },
-  {
-    "question_type": "grammar",
-    "english": "My name is Xi Xi, and I am also a cat.",
-    "grammar_point": {
-      "slug": "ye",
-      "name": "也"
+    {
+      "question_type": "reading",
+      "question": "为什么人们喜欢奶茶？",
+      "answer_options": [
+        "a. 因为奶茶太便宜了",
+        "b. 因为奶茶比咖啡因少",
+        "c. 因为奶茶好喝",
+        "d. 因为奶茶里的咖啡因多"
+      ],
+      "solution": 3
     },
-    "word_bank": [
-      "西西",
-      "我",
-      "猫",
-      "，",
-      "叫",
-      "也",
-      "我",
-      "是"
-    ],
-    "prefilled_postfix": ["。"],
-    "solutions": [
-      ["我", "叫", "西西", "，", "我", "也", "是", "猫", "。"]
-    ]
-  },
-  {
-    "question_type": "grammar",
-    "english": "I am also smiling.",
-    "grammar_point": {
-      "slug": "zhe",
-      "name": "Continuous action or state with 着"
+    {
+      "question_type": "grammar",
+      "english": "Each kind of milk tea has as much caffeine as coffee.",
+      "grammar_point": {
+        "slug": "yiyang",
+        "name": "Comparing with 一样"
+      },
+      "word_bank": [
+        "跟",
+        "里",
+        "的",
+        "多",
+        "每",
+        "一样",
+        "咖啡",
+        "咖啡因",
+        "一种",
+        "都",
+        "奶茶"
+      ],
+      "prefilled_postfix": [
+        "。"
+      ],
+      "solutions": [
+        [
+          "每",
+          "一种",
+          "奶茶",
+          "里",
+          "的",
+          "咖啡因",
+          "都",
+          "跟",
+          "咖啡",
+          "一样",
+          "多",
+          "。"
+        ]
+      ]
+    }
+  ],
+  "1467": [
+    {
+      "question_type": "reading",
+      "question": "谁住在瑞典？",
+      "answer_options": [
+        "a. 西西",
+        "b. 高小月",
+        "c. 大北",
+        "d. 妈妈"
+      ],
+      "solution": 2
     },
-    "word_bank": [
-      "在",
-      "笑",
-      "我",
-      "也",
-      "着"
-    ],
-    "prefilled_postfix": ["。"],
-    "solutions": [
-      ["我", "也", "在", "笑", "着", "。"]
-    ]
-  },
-  {
-    "question_type": "grammar",
-    "english": "I wait for him every day.",
-    "grammar_point": {
-      "slug": "dou",
-      "name": "都"
+    {
+      "question_type": "reading",
+      "question": "西西的爸爸为什么来日本？",
+      "answer_options": [
+        "a. 看西西",
+        "b. 看妈妈",
+        "c. 过生日",
+        "d. 工作"
+      ],
+      "solution": 2
     },
-    "word_bank": [
-      "我",
-      "等",
-      "都",
-      "每天",
-      "他",
-    ],
-    "prefilled_postfix": ["。"],
-    "solutions": [
-      ["我", "每天", "都", "等", "他", "。"],
-      ["每天", "我", "都", "等", "他", "。"]
-    ]
-  },
-  {
-    "question_type": "grammar",
-    "english": "I went to watch a movie yesterday.",
-    "grammar_point": {
-      "slug": "le-change",
-      "name": "了 to indicate change"
+    {
+      "question_type": "reading",
+      "question": "为什么西西的妈妈很高兴?",
+      "answer_options": [
+        "a. 因为西西在笑",
+        "b. 因为今天是她的生日",
+        "c. 因为她在看电视",
+        "d. 因为西西的爸爸来了"
+      ],
+      "solution": 3
     },
-    "word_bank": [
-      "昨天",
-      "我",
-      "去",
-      "看",
-      "电影",
-      "了"
-    ],
-    "prefilled_postfix": ["。"],
-    "solutions": [
-      ["昨天", "我", "去", "看", "电影", "了", "。"],
-      ["我", "昨天", "去", "看", "电影", "了", "。"],
-    ]
-  }
-]
+    {
+      "question_type": "grammar",
+      "english": "Mom is there smiling.",
+      "grammar_point": {
+        "slug": "zhe",
+        "name": "Continuous action or state with 着"
+      },
+      "word_bank": [
+        "着",
+        "在",
+        "那儿",
+        "妈妈",
+        "笑"
+      ],
+      "prefilled_postfix": [
+        "。"
+      ],
+      "solutions": [
+        [
+          "妈妈",
+          "在",
+          "那儿",
+          "笑",
+          "着",
+          "。"
+        ]
+      ]
+    },
+    {
+      "question_type": "grammar",
+      "english": "Dad is looking at Mom and smiling.",
+      "grammar_point": {
+        "slug": "zhe",
+        "name": "Continuous action or state with 着"
+      },
+      "word_bank": [
+        "着",
+        "笑",
+        "爸爸",
+        "看",
+        "着",
+        "妈妈"
+      ],
+      "prefilled_postfix": [
+        "。"
+      ],
+      "solutions": [
+        [
+          "爸爸",
+          "看",
+          "着",
+          "妈妈",
+          "笑",
+          "着",
+          "。"
+        ],
+        [
+          "爸爸",
+          "笑",
+          "着",
+          "看",
+          "着",
+          "妈妈",
+          "。"
+        ]
+      ]
+    },
+    {
+      "question_type": "grammar",
+      "english": "I am also smiling.",
+      "grammar_point": {
+        "slug": "zhe",
+        "name": "Continuous action or state with 着"
+      },
+      "word_bank": [
+        "也",
+        "我",
+        "笑",
+        "着",
+        "在"
+      ],
+      "prefilled_postfix": [
+        "。"
+      ],
+      "solutions": [
+        [
+          "我",
+          "也",
+          "在",
+          "笑",
+          "着",
+          "。"
+        ]
+      ]
+    },
+    {
+      "question_type": "grammar",
+      "english": "My name is Xi Xi, and I am also a cat.",
+      "grammar_point": {
+        "slug": "ye",
+        "name": "也"
+      },
+      "word_bank": [
+        "我",
+        "西西",
+        "我",
+        "叫",
+        "猫",
+        "，",
+        "也",
+        "是"
+      ],
+      "prefilled_postfix": [
+        "。"
+      ],
+      "solutions": [
+        [
+          "我",
+          "叫",
+          "西西",
+          "，",
+          "我",
+          "也",
+          "是",
+          "猫",
+          "。"
+        ]
+      ]
+    },
+    {
+      "question_type": "grammar",
+      "english": "My home is in Japan, and my dad's home is also in Japan.",
+      "grammar_point": {
+        "slug": "ye",
+        "name": "也"
+      },
+      "word_bank": [
+        "在",
+        "的",
+        "，",
+        "也",
+        "在",
+        "日本",
+        "家",
+        "的",
+        "家",
+        "爸爸",
+        "我",
+        "日本"
+      ],
+      "prefilled_postfix": [
+        "。"
+      ],
+      "solutions": [
+        [
+          "我",
+          "的",
+          "家",
+          "在",
+          "日本",
+          "，",
+          "爸爸",
+          "的",
+          "家",
+          "也",
+          "在",
+          "日本",
+          "。"
+        ]
+      ]
+    },
+    {
+      "question_type": "grammar",
+      "english": "Mom is smiling, and Dad is also smiling.",
+      "grammar_point": {
+        "slug": "ye",
+        "name": "也"
+      },
+      "word_bank": [
+        "在",
+        "，",
+        "也",
+        "在",
+        "爸爸",
+        "妈妈",
+        "笑",
+        "笑"
+      ],
+      "prefilled_postfix": [
+        "。"
+      ],
+      "solutions": [
+        [
+          "妈妈",
+          "在",
+          "笑",
+          "，",
+          "爸爸",
+          "也",
+          "在",
+          "笑",
+          "。"
+        ],
+        [
+          "爸爸",
+          "在",
+          "笑",
+          "，",
+          "妈妈",
+          "也",
+          "在",
+          "笑",
+          "。"
+        ]
+      ]
+    }
+  ],
+  "2359": [
+    {
+      "question_type": "reading",
+      "question": "为什么李小北没有女朋友？",
+      "answer_options": [
+        "a. 他的眼睛不好",
+        "b. 他不喜欢Anne",
+        "c. 他妈妈说他的课太多了",
+        "d. 他去了电影院"
+      ],
+      "solution": 2
+    },
+    {
+      "question_type": "reading",
+      "question": "李小北怎么样？",
+      "answer_options": [
+        "a. 他喜欢Anne",
+        "b. 他有很多课",
+        "c. 他有女朋友",
+        "d. 他不喜欢高天"
+      ],
+      "solution": 1
+    },
+    {
+      "question_type": "grammar",
+      "english": "Mom said Gao Tian called Anne.",
+      "grammar_point": {
+        "slug": "gei",
+        "name": "给"
+      },
+      "word_bank": [
+        "打电话",
+        "Anne",
+        "说",
+        "高天",
+        "妈妈",
+        "给"
+      ],
+      "prefilled_postfix": [
+        "。"
+      ],
+      "solutions": [
+        [
+          "妈妈",
+          "说",
+          "高天",
+          "给",
+          "Anne",
+          "打电话",
+          "。"
+        ],
+        [
+          "妈妈",
+          "说",
+          "高天",
+          "打电话",
+          "给",
+          "Anne",
+          "。"
+        ]
+      ]
+    }
+  ],
+  "901": [
+    {
+      "question_type": "reading",
+      "question": "作者为什么对椅子进行了改装？",
+      "answer_options": [
+        "a. 客户要求改装",
+        "b. 作者不想离开椅子",
+        "c. 椅子不舒服",
+        "d. 椅子太小了"
+      ],
+      "solution": 1
+    },
+    {
+      "question_type": "reading",
+      "question": "关于椅子的改装，下面说法正确的是?",
+      "answer_options": [
+        "a. 椅子改装后变得非常便宜",
+        "b. 椅子改装后有很多空间可以藏人",
+        "c. 椅子改装后不能坐人",
+        "d. 椅子改装后需要用钥匙打开"
+      ],
+      "solution": 1
+    }
+  ],
+  "95": [
+    {
+      "question_type": "reading",
+      "question": "黑木先生为什么建花园？",
+      "answer_options": [
+        "a. 买给黑木太太生日礼物",
+        "b. 为黑木太太的健康",
+        "c. 让黑木太太开心",
+        "d. 让很多人来旅游"
+      ],
+      "solution": 2
+    }
+  ],
+  "835": [
+    {
+      "question_type": "reading",
+      "question": "本开始时说了什么？",
+      "answer_options": [
+        "a. 我想二奶",
+        "b. 我要二奶",
+        "c. 我要奶",
+        "d. 我想奶"
+      ],
+      "solution": 1
+    },
+    {
+      "question_type": "reading",
+      "question": "为什么丽丽说本不能说“我要二奶”？",
+      "answer_options": [
+        "a. 服务员不卖牛奶",
+        "b. 二奶是“小老婆”的意思",
+        "c. 二奶是“妈妈”的意思",
+        "d. 本已经有了两个牛奶"
+      ],
+      "solution": 1
+    },
+    {
+      "question_type": "grammar",
+      "english": "I want two glasses of milk.",
+      "grammar_point": {
+        "slug": "yao",
+        "name": "要"
+      },
+      "word_bank": [
+        "我",
+        "要",
+        "两",
+        "杯",
+        "牛奶"
+      ],
+      "prefilled_postfix": [
+        "。"
+      ],
+      "solutions": [
+        [
+          "我",
+          "要",
+          "两",
+          "杯",
+          "牛奶",
+          "。"
+        ]
+      ]
+    },
+    {
+      "question_type": "grammar",
+      "english": "He wants a glass of water.",
+      "grammar_point": {
+        "slug": "yao",
+        "name": "要"
+      },
+      "word_bank": [
+        "要",
+        "水",
+        "他",
+        "一",
+        "杯"
+      ],
+      "prefilled_postfix": [
+        "。"
+      ],
+      "solutions": [
+        [
+          "他",
+          "要",
+          "一",
+          "杯",
+          "水",
+          "。"
+        ]
+      ]
+    },
+    {
+      "question_type": "grammar",
+      "english": "I want two glasses of milk.",
+      "grammar_point": {
+        "slug": "er-liang",
+        "name": "二 and 两"
+      },
+      "word_bank": [
+        "要",
+        "我",
+        "牛奶",
+        "两",
+        "杯"
+      ],
+      "prefilled_postfix": [
+        "。"
+      ],
+      "solutions": [
+        [
+          "我",
+          "要",
+          "两",
+          "杯",
+          "牛奶",
+          "。"
+        ]
+      ]
+    },
+    {
+      "question_type": "grammar",
+      "english": "I want the second glass of milk.",
+      "grammar_point": {
+        "slug": "er-liang",
+        "name": "二 and 两"
+      },
+      "word_bank": [
+        "第二",
+        "要",
+        "杯",
+        "奶",
+        "我"
+      ],
+      "prefilled_postfix": [
+        "。"
+      ],
+      "solutions": [
+        [
+          "我",
+          "要",
+          "第二",
+          "杯",
+          "奶",
+          "。"
+        ]
+      ]
+    },
+    {
+      "question_type": "grammar",
+      "english": "There are two friends here.",
+      "grammar_point": {
+        "slug": "er-liang",
+        "name": "二 and 两"
+      },
+      "word_bank": [
+        "朋友",
+        "有",
+        "两",
+        "个",
+        "这里"
+      ],
+      "prefilled_postfix": [
+        "。"
+      ],
+      "solutions": [
+        [
+          "这里",
+          "有",
+          "两",
+          "个",
+          "朋友",
+          "。"
+        ]
+      ]
+    }
+  ],
+  "82": [
+    {
+      "question_type": "reading",
+      "question": "比赛后，小狗得了第几名？",
+      "answer_options": [
+        "a. 第一名",
+        "b. 第三名",
+        "c. 第七名",
+        "d. 第十名"
+      ],
+      "solution": 2
+    }
+  ],
+  "082": [
+    {
+      "question_type": "grammar",
+      "english": "The little dog will run again tomorrow.",
+      "grammar_point": {
+        "slug": "hui-future",
+        "name": "Talking about the future with 会"
+      },
+      "word_bank": [
+        "跑",
+        "再",
+        "小狗",
+        "会",
+        "明天"
+      ],
+      "prefilled_postfix": [
+        "。"
+      ],
+      "solutions": [
+        [
+          "小狗",
+          "明天",
+          "会",
+          "再",
+          "跑",
+          "。"
+        ],
+        [
+          "小狗",
+          "明天",
+          "会",
+          "再",
+          "跑",
+          "。"
+        ],
+        [
+          "明天",
+          "小狗",
+          "会",
+          "再",
+          "跑",
+          "。"
+        ]
+      ]
+    },
+    {
+      "question_type": "grammar",
+      "english": "We will go to watch the marathon tomorrow.",
+      "grammar_point": {
+        "slug": "hui-future",
+        "name": "Talking about the future with 会"
+      },
+      "word_bank": [
+        "马拉松",
+        "会",
+        "去",
+        "我们",
+        "看",
+        "明天"
+      ],
+      "prefilled_postfix": [
+        "。"
+      ],
+      "solutions": [
+        [
+          "我们",
+          "明天",
+          "会",
+          "去",
+          "看",
+          "马拉松",
+          "。"
+        ],
+        [
+          "明天",
+          "我们",
+          "会",
+          "去",
+          "看",
+          "马拉松",
+          "。"
+        ],
+        [
+          "我们",
+          "会",
+          "明天",
+          "去",
+          "看",
+          "马拉松",
+          "。"
+        ]
+      ]
+    },
+    {
+      "question_type": "grammar",
+      "english": "The puppy got the seventh place.",
+      "grammar_point": {
+        "slug": "ordinals",
+        "name": "Ordinal numbers"
+      },
+      "word_bank": [
+        "第",
+        "了",
+        "小狗",
+        "名",
+        "七",
+        "得"
+      ],
+      "prefilled_postfix": [
+        "。"
+      ],
+      "solutions": [
+        [
+          "小狗",
+          "得",
+          "了",
+          "第",
+          "七",
+          "名",
+          "。"
+        ],
+        [
+          "小狗",
+          "得",
+          "第",
+          "七",
+          "名",
+          "了",
+          "。"
+        ]
+      ]
+    },
+    {
+      "question_type": "grammar",
+      "english": "Everyone is at the start line of the marathon.",
+      "grammar_point": {
+        "slug": "ordinals",
+        "name": "Ordinal numbers"
+      },
+      "word_bank": [
+        "比赛",
+        "马拉松",
+        "起点",
+        "在",
+        "的",
+        "大家"
+      ],
+      "prefilled_postfix": [
+        "。"
+      ],
+      "solutions": [
+        [
+          "大家",
+          "在",
+          "马拉松",
+          "比赛",
+          "的",
+          "起点",
+          "。"
+        ]
+      ]
+    }
+  ]
+};
