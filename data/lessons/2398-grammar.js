29199fb6e72502dad4cbcaa8a706b64b624b6866
Frithjof <fmpetrick@gmail.com>
2024-07-28 07:46:23 +0300

    data, week 2

---
+++ data/lessons/2398-grammar.js
@@ -0,0 +1,179 @@
+exerciseList = [
+  {
+    "question_type": "grammar",
+    "english": "The snow is falling heavier and heavier.",
+    "grammar_point": {
+      "slug": "yuelaiyue",
+      "name": "More and more with 越来越"
+    },
+    "word_bank": [
+      "下",
+      "越",
+      "雪",
+      "越",
+      "大"
+    ],
+    "prefilled_postfix": [
+      "。"
+    ],
+    "solutions": [
+      [
+        "雪",
+        "越",
+        "下",
+        "越",
+        "大",
+        "。"
+      ]
+    ]
+  },
+  {
+    "question_type": "grammar",
+    "english": "The room is getting colder and colder.",
+    "grammar_point": {
+      "slug": "yuelaiyue",
+      "name": "More and more with 越来越"
+    },
+    "word_bank": [
+      "房间",
+      "里",
+      "越来越",
+      "冷"
+    ],
+    "prefilled_postfix": [
+      "。"
+    ],
+    "solutions": [
+      [
+        "房间",
+        "里",
+        "越来越",
+        "冷",
+        "。"
+      ]
+    ]
+  },
+  {
+    "question_type": "grammar",
+    "english": "He doubts the authenticity of the letter more and more.",
+    "grammar_point": {
+      "slug": "yuelaiyue",
+      "name": "More and more with 越来越"
+    },
+    "word_bank": [
+      "信的真实性",
+      "越来越",
+      "他",
+      "怀疑"
+    ],
+    "prefilled_postfix": [
+      "。"
+    ],
+    "solutions": [
+      [
+        "他",
+        "越来越",
+        "怀疑",
+        "信的真实性",
+        "。"
+      ]
+    ]
+  },
+  {
+    "question_type": "grammar",
+    "english": "He put the letter on the table.",
+    "grammar_point": {
+      "slug": "ba",
+      "name": "把"
+    },
+    "word_bank": [
+      "他",
+      "把",
+      "了",
+      "桌子上",
+      "放在",
+      "信"
+    ],
+    "prefilled_postfix": [
+      "。"
+    ],
+    "solutions": [
+      [
+        "他",
+        "把",
+        "信",
+        "放在",
+        "了",
+        "桌子上",
+        "。"
+      ],
+      [
+        "他",
+        "把",
+        "信",
+        "放在",
+        "桌子上",
+        "了",
+        "。"
+      ]
+    ]
+  },
+  {
+    "question_type": "grammar",
+    "english": "Bao Zheng showed the letter to Gongsun Ce.",
+    "grammar_point": {
+      "slug": "ba",
+      "name": "把"
+    },
+    "word_bank": [
+      "包拯",
+      "把",
+      "给",
+      "公孙策",
+      "信",
+      "看"
+    ],
+    "prefilled_postfix": [
+      "。"
+    ],
+    "solutions": [
+      [
+        "包拯",
+        "把",
+        "信",
+        "给",
+        "公孙策",
+        "看",
+        "。"
+      ]
+    ]
+  },
+  {
+    "question_type": "grammar",
+    "english": "Snow water wet the clothes and shoes.",
+    "grammar_point": {
+      "slug": "ba",
+      "name": "把"
+    },
+    "word_bank": [
+      "衣服和鞋子",
+      "把",
+      "了",
+      "打湿",
+      "雪水"
+    ],
+    "prefilled_postfix": [
+      "。"
+    ],
+    "solutions": [
+      [
+        "雪水",
+        "把",
+        "衣服和鞋子",
+        "打湿",
+        "了",
+        "。"
+      ]
+    ]
+  }
+];
