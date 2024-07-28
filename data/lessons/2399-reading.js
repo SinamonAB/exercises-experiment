764a05fa2c510a72d5c1a007176a921defd9bf48
Frithjof <fmpetrick@gmail.com>
2024-07-28 07:46:23 +0300

    data, week 2

---
+++ data/lessons/2399-reading.js
@@ -0,0 +1,35 @@
+exerciseList = [
+  {
+    "question_type": "reading",
+    "question": "珠花的主人是谁？",
+    "answer_options": [
+      "a. 先皇",
+      "b. 现在的太后",
+      "c. 楚珠",
+      "d. 李妃"
+    ],
+    "solution": 2
+  },
+  {
+    "question_type": "reading",
+    "question": "关于珠花的说法，不正确的是?",
+    "answer_options": [
+      "a. 珠花是先皇在世时制作的",
+      "b. 紫珠是先皇送给楚珠的",
+      "c. 珠花上的珍珠来自东海县",
+      "d. 珠花上只有一颗紫色珍珠"
+    ],
+    "solution": 1
+  },
+  {
+    "question_type": "reading",
+    "question": "关于楚珠，下列哪项不正确?",
+    "answer_options": [
+      "a. 她是一名宫女",
+      "b. 她是南方楚地人",
+      "c. 她的名字和珍珠有关",
+      "d. 她是先皇非常喜欢的贵妃"
+    ],
+    "solution": 3
+  }
+];
