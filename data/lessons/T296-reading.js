94baa8c390dc1a3764f6a097af484dd19b2aec64
Frithjof <fmpetrick@gmail.com>
2024-07-31 22:21:17 +0200

    add missing exercises week 3 / 4

---
+++ data/lessons/T296-reading.js
@@ -0,0 +1,35 @@
+exerciseList = [
+  {
+    "question_type": "reading",
+    "question": "关于无人机应用领域，下列哪项说法是不正确的?",
+    "answer_options": [
+      "a. 无人机可以用于灾难救援",
+      "b. 无人机在新加坡表演中形成“春”字",
+      "c. 无人机的使用不需要实名登记",
+      "d. 无人机可用于监控传染病"
+    ],
+    "solution": 2
+  },
+  {
+    "question_type": "reading",
+    "question": "文中提到的无人机的使用不包括下面哪种情况？",
+    "answer_options": [
+      "a. 灾难救援",
+      "b. 军事侦察",
+      "c. 医疗手术",
+      "d. 新闻报道"
+    ],
+    "solution": 2
+  },
+  {
+    "question_type": "reading",
+    "question": "有关无人机的说法，下列哪项正确？",
+    "answer_options": [
+      "a. 无人机只能用于军事侦察",
+      "b. 民用无人机可以随时飞行，不需要登记",
+      "c. 无人机可作为救援工具，实现高效搜寻",
+      "d. 无人机在节日表演中使用不到"
+    ],
+    "solution": 2
+  }
+];
