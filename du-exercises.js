a0443aa03d5849e094f421f6f3fe73e0f3d7de51
Frithjof <fmpetrick@gmail.com>
2024-08-02 11:26:04 +0200

    reset root path

--- du-exercises.js
+++ du-exercises.js
@@ -12,7 +12,7 @@
 (async () => {
     "use strict";
 
-    const rootPath = "https://duchinese-exercises-staging.netlify.app";
+    const rootPath = "https://sinamonab.github.io/exercises-experiment";
 
     // https://stackoverflow.com/a/61511955/2766231
     function waitForElement(selector) {
